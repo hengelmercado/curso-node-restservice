eaf8c7cbc93fa5bb405cb0ff8b396e5b73a60498
hengelmercado <hengelmercado@gmail.com>
2021-05-14 08:37:57 -0500

    Update linux

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,7 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
 
 const usuariosGet = (req, res = response) => {
 
@@ -17,11 +20,34 @@ const usuariosPut = (req, res = response) => {
         msg: 'put API - Controller'
     });
 } 
-const usuariosPost = (req, res = response) => {
-    const body = req.body;
+const usuariosPost = async (req, res = response) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+    }
+
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo,password,rol});
+
+    // Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+    if (existeEmail) {
+        return res.status(400).json({
+            msg: 'El correo ya esta registrado'
+        })
+    }
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync(password, salt);
+
+    // Guardar en la DB
+    await usuario.save();
+
+
     res.json({
-        msg: 'post API - Controller',
-        body
+        usuario
     });
 }
 const usuariosPatch = (req, res = response) => {
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
 
 const router = Router();
 
 router.get('/', usuariosGet);
 router.put('/', usuariosPut);
-router.post('/', usuariosPost);
+router.post('/',[
+    check('correo', 'El correo no es valido').isEmail(),
+], usuariosPost);
 router.delete('/', usuariosDelete);
 router.patch('/', usuariosPatch);
 
