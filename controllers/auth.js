2f6d7202de1c7e5eeca8746740fedfb8301457bf
hengelmercado <hengelmercado@gmail.com>
2021-05-22 20:16:51 -0500

    auth google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const { response } = require("express");
 const bcryptjs = require('bcryptjs');
-const { generarJWT } = require("../helpers/generar-jwt");
 const Usuario = require("../models/usuario");
+const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 
 const login = async(req, res = response) => {
@@ -49,11 +50,46 @@ const login = async(req, res = response) => {
 
 }
 
-const googleSignIn = (req, res = response) => {
+const googleSignIn = async(req, res = response) => {
+    const { id_token } = req.body;
 
-    res.json({
-        msg: 'Todo OK googel signin'
-    })
+    try {
+
+        const { correo, nombre, img } =  await googleVerify(id_token);
+        let usuario = await Usuario.findOne({ correo });
+        if( !usuario ){
+            // Crear usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+            
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+        
+        if( !usuario.estado ){
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+        
+        // Generar el JWT
+        const token = await generarJWT(usuario.id);
+        
+        res.json({
+            usuario,
+            token
+        });
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        });
+    }
+
 }
 
 
